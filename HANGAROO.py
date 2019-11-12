d75ba041169b16cb61312f7eab530749e74803ea
Paolo Fajardo <57589064+paolofrd@users.noreply.github.com>
2019-11-12 18:00:01 +0800

    Add files via upload

---
+++ HANGAROO.py
@@ -0,0 +1,27 @@
+def Hangaroo (secretWord):
+    mistakesMade = 1
+    print ("Welcome to Hangaroo, user!")
+    print ("Guess the secret word!")
+    print ("You have 4 attempts or else the kangaroo dies.")
+    while mistakesMade > 0:
+        letters = input("Please guess the secret word or at least the letters that make up the secret word!: ")
+        letters = letters.lower()
+        lettersGuessed = list(letters)
+        if isWordGuessed(secretWord,lettersGuessed) is True:
+            print ("Congratulations! You guessed the correct word!")
+            print ("")
+            print ("Secret Word: ", secretWord)
+            mistakesMade = 0
+            break
+        elif isWordGuessed(secretWord,lettersGuessed) is False:
+            print ("Mistakes Made: ", mistakesMade)
+            if mistakesMade == 4:
+                print("CONGRATULATIONS! THE KANGAROO IS DEAD!")
+                break
+            mistakesMade += 1
+            print ("")
+            print ("Guess status: ")
+            getGuessedWord(secretWord, lettersGuessed)
+            print ("")
+            print ("Available Letters: ")
+            getAvailableLetters (lettersGuessed)
